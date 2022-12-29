115ea1a7b1b074f97ea7bdc278a9ad911fb406c0
Karima <karimasadykova@gmail.com>
2022-12-29 19:32:06 +0100

    src files modified

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,89 +1,87 @@
-#include "../include/libftprintf.h"
+#include "../include/ft_printf.h"
 
-char    *ft_flags(const char **format)
+int	ft_is_printable(char c)
 {
-    char    *flag;
-
-    flag = NULL;
-    while (**format == ' ')
-        *format += 1;
-    if (!ft_check_flag(format))
-        return (NULL);
-    while ((**format >= 0 && **format <= 9)
-        || **format == '.' || **format == '*'
-        || **format == '-')
-    {
-        ft_strjoin(flag, *format);
-        *format += 1;
-    }
-    return (flag);
+	if (c == 'd' || c == 'i'
+		|| c == 'u' || c == 'p'
+		|| c == 'c' || c == 's'
+		|| c == 'x' || c == 'X' || c == '%')
+		return (1);
+	return (0);
 }
 
-void	ft_putstr(char *str)
+void	ft_flags(const char **format, t_flags *flags)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
+	while (format && *format && **format && !ft_is_printable(**format))
 	{
-		write(1, &str[i], 1);
-		i++;
+		if (**format == '.')
+		{
+			++*format;
+			flags->point = ft_atoi_deluxe(format);
+		}
+		else if (**format == '#')
+			flags->diese = 1;
+		else if (**format == ' ')
+			flags->espace = 1;
+		else if (**format == '+')
+			flags->plus = 1;
+		else if (**format == '0')
+			flags->zero = 1;
+		else if (**format == '-')
+			flags->tiret = 1;
+		else if (**format >= '0' && **format <= '9')
+			flags->minimal_length = ft_atoi_deluxe(format);
+		++*format;
 	}
+	flags->type = **format;
+	++*format;
 }
 
-int     ft_format(va_list *argc, const char *format)
+int	ft_format(va_list *argc, const char **format)
 {
-    char    *str;
-    char    *flag;
-    int     len;
+	t_flags	flags;
 
-    if (!ft_is_printable(*format) 
-        && (flag = ft_flags(&format)) == NULL)
-            return (-1);
-    if (*format == 'd' || *format == 'i')
-        str = ft_int(argc);
-    if (*format == 'u')
-        str = ft_uint(argc);
-    if (*format == 'c')
-        str = ft_chr(argc);
-    if (*format == 's')
-        str = ft_str(&argc);
-    if (*format == 'p')
-        str = ft_ptr(&argc);
-    if (*format == 'x')
-        str = ft_hexa_low(argc);
-    if (*format == 'X')
-        str = ft_hexa_up(argc);
-    //ft_putflag(flag, str);
-    ft_putstr(str);
-    len = ft_strlen(str);
-    free(str);
-    return (len);
+	ft_initialize_flags(&flags);
+	ft_flags(format, &flags);
+	if (flags.type == 'd' || flags.type == 'i')
+		return (ft_printf_di(argc, flags));
+	if (flags.type == 'u')
+		return (ft_printf_u(argc, flags));
+	if (flags.type == 'c')
+		return (ft_printf_c(argc, flags));
+	if (flags.type == 's')
+		return (ft_printf_s(argc, flags));
+	if (flags.type == 'p')
+		return (ft_printf_p(argc, flags));
+	if (flags.type == 'x')
+		return (ft_printf_x(argc, flags, 0));
+	if (flags.type == 'X')
+		return (ft_printf_x(argc, flags, 1));
+	if (flags.type == '%')
+		return (write(1, "%%", 1), 1);
+	return (0);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list args;
-    int     nbchar;
-    char    c;
+	va_list	args;
+	int		nbchar;
+	char	c;
 
-    nbchar = 0;
-    va_start(args, format);
-    while((c = *format))
-    {
-        format++;
-        if (c == '%')
-        {
-            //int value = 0;
-            nbchar += ft_format(&args, format); 
-            format++;
-        }
-        else
-        {
-            write(1, &c, 1);
-            nbchar++;
-        }
-    }
-    va_end(args);
-    return (nbchar);
+	nbchar = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		c = *format;
+		++format;
+		if (c == '%')
+			nbchar += ft_format(&args, &format);
+		else
+		{
+			write(1, &c, 1);
+			nbchar++;
+		}
+	}
+	va_end(args);
+	return (nbchar);
 }
